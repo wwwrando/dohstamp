393b8ecbd1382ca52f3d9e9dc867119559994824
wwwrando <wwwrando>
2024-07-30 01:54:11 +0000

    minor updates

--- main.go
+++ main.go
@@ -2,55 +2,56 @@ package main
 
 import (
 	"bytes"
-	"fmt"
-	"github.com/jedisct1/go-dnsstamps"
-	"encoding/hex"
-	"encoding/pem"
-	"crypto/x509"
 	"crypto/sha256"
 	"crypto/tls"
+	"crypto/x509"
+	"encoding/hex"
+	"encoding/pem"
 	"flag"
+	"fmt"
+	"github.com/jedisct1/go-dnsstamps"
+	"strings"
 	"time"
 )
 
 func GetCertificatesPEM(address string, last bool) (string, error) {
-    conn, err := tls.Dial("tcp", address, &tls.Config{
-        InsecureSkipVerify: false,
-    })
-    if err != nil {
-        return "", err
-    }
-    defer conn.Close()
+	conn, err := tls.Dial("tcp", address, &tls.Config{
+		InsecureSkipVerify: false,
+	})
+	if err != nil {
+		return "", err
+	}
+	defer conn.Close()
 	var b bytes.Buffer
 
-	i := 0;
+	i := 0
 
 	for _, cert := range conn.ConnectionState().PeerCertificates {
 
 		/* If last is true skip the last (host) certificate */
 		if !last && i == 0 {
 			i = i + 1
-			continue;
+			continue
 		}
 
-        err := pem.Encode(&b, &pem.Block{
-            Type: "CERTIFICATE",
-            Bytes: cert.Raw,
-        })
+		err := pem.Encode(&b, &pem.Block{
+			Type:  "CERTIFICATE",
+			Bytes: cert.Raw,
+		})
 
-        if err != nil {
-            return "", err
-        }
-    }
-    return b.String(), nil
+		if err != nil {
+			return "", err
+		}
+	}
+	return b.String(), nil
 }
 
 func main() {
 
-	var sdns string;
-	var last bool;
-	var show bool;
-	var silent bool;
+	var sdns string
+	var last bool
+	var show bool
+	var silent bool
 
 	flag.StringVar(&sdns, "sdns", "", "DoH server sdns with outdated certificate")
 	flag.BoolVar(&last, "last", false, "Hash the last certificate in the chain instead of the certificate that signed the last certificate in the chain")
@@ -63,28 +64,35 @@ func main() {
 		return
 	}
 
-	stamp, _ := dnsstamps.NewServerStampFromString(sdns);
+	stamp, _ := dnsstamps.NewServerStampFromString(sdns)
+
+	var provider string
+	if strings.Contains(stamp.ProviderName, ":") {
+		provider = stamp.ProviderName
+	} else {
+		provider = stamp.ProviderName + ":443"
+	}
 
 	if !silent {
-		fmt.Println("Retrieving PEM certificate for " + stamp.ServerAddrStr + "/" + stamp.ProviderName + ":443")
+		fmt.Println("Retrieving PEM certificate for " + stamp.ServerAddrStr + "/" + provider)
 	}
 
 	if stamp.Proto != dnsstamps.StampProtoTypeDoH {
-		fmt.Println("ERROR: This is not a DoH server, refusing to proceed");
+		fmt.Println("ERROR: This is not a DoH server, refusing to proceed")
 		return
 	}
 
 	pemData, err := GetCertificatesPEM(stamp.ServerAddrStr, last)
 	if err != nil {
-		if !silent{
+		if !silent {
 			fmt.Println("WARNING: Failed retrieving the certificate using the IP address, trying DNS provider name")
 		}
 
-		pemData, err = GetCertificatesPEM(stamp.ProviderName + ":443", last)
+		pemData, err = GetCertificatesPEM(provider, last)
 		if err != nil {
-			fmt.Println("ERROR: Failed retrieving the certificate");
+			fmt.Println("ERROR: Failed retrieving the certificate")
 			if !silent {
-				fmt.Println(err);
+				fmt.Println(err)
 			}
 			return
 		}
@@ -100,12 +108,12 @@ func main() {
 		}
 	}
 
-	block, _ := pem.Decode([]byte(pemData));
+	block, _ := pem.Decode([]byte(pemData))
 
 	parsedCert, err := x509.ParseCertificate(block.Bytes)
 	if err != nil {
 		fmt.Println("ERROR: Unable to parse certificate")
-		if (!silent) {
+		if !silent {
 			fmt.Println(err)
 		}
 		return
@@ -115,21 +123,9 @@ func main() {
 		fmt.Println("Certificate issued by " + parsedCert.Issuer.String())
 		fmt.Println(parsedCert.IssuingCertificateURL)
 
-		fmt.Println("Valid from " + parsedCert.NotBefore.Format(time.RFC1123))
+		fmt.Println("Valid from  " + parsedCert.NotBefore.Format(time.RFC1123))
 		fmt.Println("Valid until " + parsedCert.NotAfter.Format(time.RFC1123))
 	}
-
-	certPool, err := x509.SystemCertPool()
-	if err != nil {
-		if !silent {
-			fmt.Println("WARNING: Unable to fetch system certificate pool")
-		}
-	}
-
-	chain, err := parsedCert.Verify(x509.VerifyOptions{
-		Roots: certPool,
-	})
-
 
 	if !silent {
 
@@ -137,9 +133,20 @@ func main() {
 
 		if !last {
 
+			certPool, err := x509.SystemCertPool()
+			if err != nil {
+				if !silent {
+					fmt.Println("WARNING: Unable to fetch system certificate pool")
+				}
+			}
+
+			chain, err := parsedCert.Verify(x509.VerifyOptions{
+				Roots: certPool,
+			})
+
 			if len(chain) == 0 {
 				fmt.Println("WARNING: Chain of trust from system certificates does not exist")
-			}else{
+			} else {
 				fmt.Println("Certificate validated using system certificate pool")
 			}
 
@@ -147,14 +154,14 @@ func main() {
 
 		if last {
 
-			var valid bool;
-			valid = true;
+			var valid bool
+			valid = true
 
 			for _, w := range parsedCert.DNSNames {
 				err = parsedCert.VerifyHostname(w)
 				if err != nil {
 					fmt.Println("WARNING: Certificated declared to be valid for " + w + " but verification not passed")
-					valid = false;
+					valid = false
 				}
 			}
 
@@ -178,24 +185,24 @@ func main() {
 		ints[i] = uint8(sha[i])
 	}
 
-	for v,w := range stamp.Hashes {
+	for v, w := range stamp.Hashes {
 		stamp.Hashes[v] = ints
 		if !silent {
 			fmt.Println("Previous hash " + hex.EncodeToString(w))
-			fmt.Println("New hash " + hex.EncodeToString(ints))
+			fmt.Println("New hash      " + hex.EncodeToString(ints))
 		}
 	}
 
 	if !silent {
-		var unchanged string;
+		var unchanged string
 		if stamp.String() == sdns {
-			unchanged = "(unchanged)";
-		}else {
+			unchanged = "(unchanged)"
+		} else {
 			unchanged = "(changed)"
 		}
-		fmt.Println("\nResult " + unchanged + ":\n" + stamp.String());
+		fmt.Println("\nResult " + unchanged + ":\n" + stamp.String())
 
-	}else{
+	} else {
 		fmt.Print(stamp.String())
 	}
 
